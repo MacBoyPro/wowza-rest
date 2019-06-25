c62a7276831a6898859d880f1760d7e9c57858b8
Lee Lundrigan <lee.lundrigan@gmail.com>
2019-06-24 18:28:14 -0700

    adding stream targets

--- lib/wowza_rest/api.rb
+++ lib/wowza_rest/api.rb
@@ -1,6 +1,7 @@
 require 'wowza_rest/applications'
 require 'wowza_rest/instances'
 require 'wowza_rest/publishers'
+require 'wowza_rest/stream_targets'
 require 'wowza_rest/smils'
 
 module WowzaRest
@@ -9,6 +10,7 @@ module WowzaRest
     include WowzaRest::Applications
     include WowzaRest::Instances
     include WowzaRest::Publishers
+    include WowzaRest::StreamTargets
     include WowzaRest::SMILs
   end
 end
